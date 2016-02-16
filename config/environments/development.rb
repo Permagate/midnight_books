fb3421bd79f19490c637ef9736a2f68d25ac78ac
Permagate <kend654@gmail.com>
2016-02-16 21:24:45 +0700

    [Dev] Add web console configuration for development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Whitelist IP for web_console.
+  config.web_console.whitelisted_ips = '10.0.2.2'
 end
